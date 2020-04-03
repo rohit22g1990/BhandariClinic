4d006b92c379d432aae46a4ce7e64478f4830d9d
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-04-03 19:25:32 +0530

    Home service changes

--- bhandariClinic/app/Services/HomeService.php
+++ bhandariClinic/app/Services/HomeService.php
@@ -9,15 +9,15 @@ class HomeService
 {
     public function getGoogleReviews()
     {
-        $client = new Client();
-        $response = $client->request('GET', 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ206X-XXAwjsRQrscY7rT9rw&fields=name,rating,formatted_phone_number,reviews&key=AIzaSyBSxYKh-RcxhaJ3_KMgijaIg0qF2x6DpbU');
-        $statusCode = $response->getStatusCode();
-
-        return $response->getBody()->getContents();
+//        $client = new Client();
+//        $response = $client->request('GET', 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ206X-XXAwjsRQrscY7rT9rw&fields=name,rating,formatted_phone_number,reviews&key=AIzaSyBSxYKh-RcxhaJ3_KMgijaIg0qF2x6DpbU');
+//        $statusCode = $response->getStatusCode();
+//
+//        return $response->getBody()->getContents();
     }
 
     public function getContactDetails()
     {
-                
+
     }
 }
